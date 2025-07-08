cf4d219bcda020b9631f1906161dfa55eed02773
kastov <kastov@gog.sh>
2025-07-08 05:32:03 +0300

    refactor: update RedisChannelConfig to use ConnectionOptions

--- src/channel/redis.channel-config.ts
+++ src/channel/redis.channel-config.ts
@@ -1,25 +1,27 @@
 import { ChannelConfig } from '@nestjstools/messaging';
+import { ConnectionOptions } from 'bullmq';
 
 export class RedisChannelConfig extends ChannelConfig {
-  public readonly connection: Connection;
+  public readonly connection: ConnectionOptions;
   public readonly queue: string;
 
   constructor({
-                name,
-                connection,
-                queue,
-                enableConsumer,
-                avoidErrorsForNotExistedHandlers,
-                middlewares,
-                normalizer,
-              }: RedisChannelConfig) {
-    super(name, avoidErrorsForNotExistedHandlers, middlewares, enableConsumer, normalizer)
+    name,
+    connection,
+    queue,
+    enableConsumer,
+    avoidErrorsForNotExistedHandlers,
+    middlewares,
+    normalizer,
+  }: RedisChannelConfig) {
+    super(
+      name,
+      avoidErrorsForNotExistedHandlers,
+      middlewares,
+      enableConsumer,
+      normalizer,
+    );
     this.connection = connection;
     this.queue = queue;
   }
-}
-
-interface Connection {
-  host: string;
-  port: number;
 }
